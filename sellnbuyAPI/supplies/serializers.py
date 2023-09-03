5f8f68b9b9d91624cfa722d7a7363147ecc81e23
Artem Usachov <ar.usa4yov@yandex.ru>
2023-09-03 09:39:48 +0300

    API for comments on supplies added

--- sellnbuyAPI/supplies/serializers.py
+++ sellnbuyAPI/supplies/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Supply, Comment, Message
+from django.shortcuts import get_object_or_404
 
 
 class SupplySerializer(serializers.ModelSerializer):
@@ -10,3 +11,18 @@ class SupplySerializer(serializers.ModelSerializer):
         fields = ('user', 'name', 'price', 'desc', 'image')
         read_only_fields = ('user', )
 
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
+
+    class Meta:
+        model = Comment
+        fields = ('user', 'rating', 'supply', 'text',)
+        read_only_fields = ('user', 'supply')
+
+    def validate(self, attrs):
+        supply = get_object_or_404(Supply, id=self.context['view'].kwargs.get('id'))
+        comment_exist = Comment.objects.filter(supply=supply)
+        if comment_exist:
+            raise serializers.ValidationError
+        return attrs
--- sellnbuyAPI/supplies/views.py
+++ sellnbuyAPI/supplies/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from .serializers import SupplySerializer
+from django.shortcuts import render, get_object_or_404
+from .serializers import SupplySerializer, CommentSerializer
 from rest_framework import viewsets, mixins
-from .models import Supply
+from .models import Supply, Comment
 
 
 class SupplyViewSet(viewsets.ModelViewSet):
@@ -10,3 +10,17 @@ class SupplyViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        supply = get_object_or_404(Supply, id=self.kwargs.get('id'))
+        return Comment.objects.filter(supply=supply)
+
+    def perform_create(self, serializer):
+        supply = get_object_or_404(Supply, id=self.kwargs.get('id'))
+        serializer.save(user=self.request.user, supply=supply)
+    
